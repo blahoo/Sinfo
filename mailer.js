412aad595997ae7c776d9716571728e9233784d0
blahoo <117117475+blahoo@users.noreply.github.com>
2023-09-07 19:55:08 -0400

    clean up reop and add nodemailer

--- mailer.js
+++ mailer.js
@@ -30,6 +30,40 @@ emailInput.addEventListener('input', () => {
 
 submitButton.addEventListener('submit', function(event) {
     event.preventDefault();
-    email = emailInput.value;
-    console.log(email)
+    let email_value = emailInput.value;
+    let name_value = nameInput.value 
+    console.log(email_value)
+    console.log(name_value)
+    const nodemailer = require('nodemailer');
+
+    // Create a transporter object using Gmail's SMTP server
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+        type: 'OAuth2',
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN
+        }
+    });
+
+    // Email content
+    const mailOptions = {
+        from: '1gugre@gmail.com',
+        to: email_value,
+        subject: 'Automated Email',
+        text: 'Hello, ' + name_value + '\n  Your email has been captured and collected'
+    };
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error sending email:', error);
+        } else {
+            console.log('Email sent:', info.response);
+        }
+    });
+
 });
